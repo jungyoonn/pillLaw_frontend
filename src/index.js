9339c3c6dc23530ef62ebc8a2dc737e4706a4493
jungyoonn <sophia7625@naver.com>
2025-02-08 20:40:55 +0900

    경로 수정

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TestEditor from './component/common/hadmin/ApiTest/TestEditor';
+import TestEditor from './component/hadmin/test/TestEditor'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
