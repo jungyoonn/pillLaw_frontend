1b71ec9eb06dc259e6fefaa180b22982f86cfd13
DahnDell <khanman1208@gmail.com>
2025-02-10 15:22:28 +0900

    App.js 변경사항 커밋(라우트)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import Headerr from './component/common/Headerr';
 import Index from './component/common/Index';
 // import FollowList from './component/follow/followList';
-import Test from './component/follow/Test';
+// import Test from './component/follow/Test';
 import Footer from './component/common/Footer';
+import ProductList from './component/product/ProductList';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Headerr />
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path='follow' element={<Test />} />
+        <Route path='/product/list' element={<ProductList />} />
         {/* <Route path="/posts" element={<Posts />} />
         <Route path="/posts/:id" element={<PostDetail />} /> */}
       </Routes>
