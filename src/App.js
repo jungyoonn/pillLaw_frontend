7b9151e00370cbf2e33674deae75252e994266d4
jungyoonn <sophia7625@naver.com>
2025-02-10 12:09:59 +0900

    router 틀

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Headerr from './component/common/Headerr';
+import Index from './component/common/Index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Headerr />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        {/* <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:id" element={<PostDetail />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TestEditor from './component/hadmin/test/TestEditor'
+import App from './App';
+// import TestEditor from './component/hadmin/test/TestEditor'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TestEditor />
+    <App />
   </React.StrictMode>
 );
 
