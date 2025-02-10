c09af620ff6f1fb42680a68d79a8714ea5dd8086
mawoo-UI <hanjutun@gmail.com>
2025-02-10 14:20:16 +0900

    테스트용 jsx

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Headerr from './component/common/Headerr';
 import Index from './component/common/Index';
+import FollowList from './component/follow/followList';
+import Test from './component/follow/Test';
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Headerr />
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path='follow' element={<Test />} />
         {/* <Route path="/posts" element={<Posts />} />
         <Route path="/posts/:id" element={<PostDetail />} /> */}
       </Routes>
---
+++ src/component/follow/Test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FollowList from './followList';
+
+const Test = () => {
+  return (
+    <FollowList />
+  );
+}
+
+export default Test;
