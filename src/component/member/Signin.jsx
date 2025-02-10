882dd5df454fe04560a64c650897696bc7fd267f
jungyoonn <sophia7625@naver.com>
2025-02-10 19:31:41 +0900

    Link > Button

--- src/component/member/Signin.jsx
+++ src/component/member/Signin.jsx
@@ -5,12 +5,11 @@ import logo from '../../resources/image/pilllaw_icon_crop.png';
 import kakao from '../../resources/image/kakao-svgrepo-com.png';
 import naver from '../../resources/image/naver-svgrepo-com.png';
 import google from '../../resources/image/google-color-svgrepo-com.png';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import Button from '../common/Button';
+import { Container, Row, Col, Form } from 'react-bootstrap';
 
 const Signin = () => {
   return (
@@ -36,63 +35,61 @@ const Signin = () => {
         <div className="mx-5 px-5 mt-4">
           <div className="mx-5 px-5">
             <div className="mx-5 px-5 mt-3">
-              <form className="px-5 mx-5" method="post">
-                <div className="mt-3 mx-5 px-5">
-                  <label htmlFor="email" className="form-label"></label>
-                  <input type="email" className="form-control border-bottom" id="email" placeholder="이메일" name="email" />
-                </div>
-                <div className="mx-5 px-5 mt-1">
-                  <input type="password" className="form-control" id="pwd" placeholder="비밀번호" name="pswd" />
-                </div>
-                <div className="form-check mb-3 mx-5 px-5">
-                  <label className="form-check-label header-font fw-bold fs-12">
-                    <input className="form-check-input mx-2" type="checkbox" name="remember" /> 로그인 정보 기억하기
-                  </label>
-                </div>
+              <Form className="px-5 mx-5" method="post">
+                <Form.Group className="mt-3 mx-5 px-5">
+                  <Form.Label htmlFor="email"></Form.Label>
+                  <Form.Control type="email" id="email" placeholder="이메일" name="email" />
+                </Form.Group>
+                <Form.Group className="mx-5 px-5 mt-1">
+                  <Form.Control type="password" id="pwd" placeholder="비밀번호" name="pswd" />
+                </Form.Group>
+                <Form.Group className="mb-3 mx-5 px-5">
+                  <Form.Label className="header-font fw-bold fs-12">
+                    <Form.Check type="switch" className="mx-2" label="로그인 정보 기억하기" name="remember" />
+                  </Form.Label>
+                </Form.Group>
                 <div className="d-grid px-4 mx-4">
-                  <button type="submit" className="btn btn-pilllaw mx-5 btn-block">로그인</button>
+                  <Button  variant="pilllaw" type="submit" className="btn btn-pilllaw mx-5 btn-block d-grid">로그인</Button>
                 </div>
-              </form>
+              </Form>
 
               <div className="mx-4 px-4">
-                <div className="mt-5"></div>
-
                 {/* 카카오 로그인 */}
-                <div className="d-grid px-5 mx-5 mt-3">
-                  <Link to={"/"} className="btn btn-kakao mx-5 btn-block fw-bold text-kakao py-1">
+                <div className="d-grid px-5 mx-5 mt-5">
+                  <Button variant='kakao' to={"/"} className="btn btn-kakao mx-5 btn-block fw-bold text-kakao pt-2 fs-14">
                     <img className="img-fluid me-2 mb-1" src={kakao} width="18" alt="카카오 로그인" />
                     카카오 로그인
-                  </Link>
+                  </Button>
                 </div>
 
                 {/* 네이버 로그인 */}
                 <div className="d-grid px-5 mx-5 mt-2">
-                  <Link to={"/"} className="btn btn-naver mx-5 btn-block fw-bold text-white py-1">
+                  <Button variant='naver' to={"/"} className="btn btn-naver mx-5 btn-block fw-bold text-white pt-2 fs-14">
                     <img className="img-fluid me-2 mb-1" src={naver} width="18" alt="네이버 로그인" />
                     네이버 로그인
-                  </Link>
+                  </Button>
                 </div>
 
                 {/* GitHub 로그인 */}
                 <div className="d-grid px-5 mx-5 mt-2">
-                  <Link to={"/"} className="btn btn-github mx-5 fw-bold text-white mb-1">
+                  <Button variant='github' to={"/"} className="btn btn-github mx-5 fw-bold text-white mb-1 fs-14 py-2">
                     <FontAwesomeIcon icon={faGithub} className="mx-2 fa-lg" /> GitHub 로그인
-                  </Link>
+                  </Button>
                 </div>
 
                 {/* 인스타그램 로그인 */}
                 <div className="d-grid px-5 mx-5 mt-1">
-                  <Link to={"/"} className="btn btn-insta mx-5 fw-bold text-white mb-1">
+                  <Button variant='insta' to={"/"} className="btn btn-insta mx-5 fw-bold text-white mb-1 fs-14 py-2">
                     <FontAwesomeIcon icon={faInstagram} className="mx-2 ms-4 fa-lg" /> 인스타그램 로그인
-                  </Link>
+                  </Button>
                 </div>
 
                 {/* Google 로그인 */}
                 <div className="d-grid px-5 mx-5 mt-1">
-                  <Link to={"/"} className="btn btn-google mx-5 fw-bold mb-1">
+                  <Button variant='google' to={"/"} className="btn btn-google mx-5 fw-bold mb-1 fs-14 py-2">
                     <img className="img-fluid me-2 mb-1" src={google} width="18" alt="구글 로그인" />
                     Google 로그인
-                  </Link>
+                  </Button>
                 </div>
               </div>
             </div>
--- src/component/member/Signup.jsx
+++ src/component/member/Signup.jsx
@@ -5,13 +5,12 @@ import logo from '../../resources/image/pilllaw_icon_crop.png';
 import kakao from '../../resources/image/kakao-svgrepo-com.png';
 import naver from '../../resources/image/naver-svgrepo-com.png';
 import google from '../../resources/image/google-color-svgrepo-com.png';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Button from '../common/Button';
 
 const Signup = () => {
   return (
@@ -49,13 +48,13 @@ const Signup = () => {
               <div className="mx-4 px-4">
                 <div className="mt-5"></div>
                 <div className="d-grid px-5 mx-5 mt-3">
-                  <Link to={"/"} className="btn btn-pilllaw mx-5 btn-block fw-bold text-white">
+                  <Button variant='pilllaw' to={"/signup/terms"} className="btn btn-pilllaw mx-5 btn-block fw-bold text-white fs-14 py-2">
                     <FontAwesomeIcon icon={faEnvelope} className="fa-lg mx-2" />
                     이메일로 시작하기
-                  </Link>
+                  </Button>
                 </div>
                 <div className="d-grid px-5 mx-5 mt-2">
-                  <Link to={"/"} className="btn btn-kakao mx-5 btn-block fw-bold text-kakao p-1">
+                  <Button variant='kakao' to={"/"} className="btn btn-kakao mx-5 btn-block fw-bold text-kakao p-1 fs-14 pt-2">
                     <img
                       className="img-fluid me-2 mb-1"
                       src={kakao}
@@ -63,10 +62,10 @@ const Signup = () => {
                       alt="카카오 회원가입"
                     />
                     카카오로 시작하기
-                  </Link>
+                  </Button>
                 </div>
-                <div className="d-grid px-5 mx-5 mt-2">
-                  <Link to={"/"} className="btn btn-naver mx-5 btn-block fw-bold text-white py-1">
+                <div className="d-grid px-5 mx-5 mt-2 fs-12 align-items-center">
+                  <Button variant='naver' to={"/"} className="btn btn-naver mx-5 btn-block fw-bold text-white pt-2 fs-14">
                     <img
                       className="img-fluid me-2 mb-1"
                       src={naver}
@@ -74,22 +73,22 @@ const Signup = () => {
                       alt="네이버 회원가입"
                     />
                     네이버로 시작하기
-                  </Link>
+                  </Button>
                 </div>
-                <div className="d-grid px-5 mx-5 mt-2">
-                  <Link to={"/"} className="btn btn-github mx-5 fw-bold text-white mb-1">
+                <div className="d-grid px-5 mx-5 mt-2 fs-12">
+                  <Button variant='github' to={"/"} className="btn btn-github mx-5 fw-bold text-white mb-1 fs-14 py-2">
                     <FontAwesomeIcon icon={faGithub} className="fa-lg mx-2" />
                     GitHub로 시작하기
-                  </Link>
+                  </Button>
                 </div>
-                <div className="d-grid px-5 mx-5 mt-1">
-                  <Link to={"/"} className="btn btn-insta mx-5 fw-bold text-white mb-1">
+                <div className="d-grid px-5 mx-5 mt-1 fs-12">
+                  <Button variant='insta' to={"/"} className="btn btn-insta mx-5 fw-bold text-white mb-1 fs-14 py-2">
                     <FontAwesomeIcon icon={faInstagram} className="fa-lg mx-2 ms-4" />
                     인스타그램으로 시작하기
-                  </Link>
+                  </Button>
                 </div>
-                <div className="d-grid px-5 mx-5 mt-1">
-                  <Link to={"/"} className="btn btn-google mx-5 fw-bold mb-1">
+                <div className="d-grid px-5 mx-5 mt-1 fs-12">
+                  <Button variant='google' to={"/"} className="btn btn-google mx-5 fw-bold mb-1 fs-14 py-2">
                     <img
                       className="img-fluid me-2 mb-1"
                       src={google}
@@ -97,28 +96,28 @@ const Signup = () => {
                       alt="구글 회원가입"
                     />
                     Google로 시작하기
-                  </Link>
+                  </Button>
                 </div>
               </div>
             </div>
             <hr className="mt-5" />
-            <div className="row text-center clearfix">
-              <p className="col fw-bold fs-12 mx-3">
-                <a className="text-decoration-none text-pilllaw" href="#">
+            <Row className="text-center">
+              <Col className="fw-bold fs-12 mx-3">
+                <Link to={"/"} className="text-decoration-none text-pilllaw">
                   개인정보 처리방침
-                </a>
-              </p>
-              <p className="col fw-bold fs-12 mx-3">
-                <a className="text-decoration-none text-pilllaw" href="#">
+                </Link>
+              </Col>
+              <Col className="fw-bold fs-12 mx-3">
+                <Link to={"/"} className="text-decoration-none text-pilllaw">
                   이용 약관
-                </a>
-              </p>
-              <p className="col fw-bold fs-12 mx-3">
-                <a className="text-decoration-none text-pilllaw" href="#">
+                </Link>
+              </Col>
+              <Col className="fw-bold fs-12 mx-3">
+                <Link to={"/"} className="text-decoration-none text-pilllaw">
                   이용 안내
-                </a>
-              </p>
-            </div>
+                </Link>
+              </Col>
+            </Row>
           </div>
         </div>
       </Container>
