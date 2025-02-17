b96e7650ae800cba22e9ef0be25234a5157ccf5f
jungyoonn <sophia7625@naver.com>
2025-02-17 18:10:45 +0900

    로그인 시 토큰 체크하지 않도록 수정

--- src/hooks/UseAxios.jsx
+++ src/hooks/UseAxios.jsx
@@ -15,20 +15,25 @@ const UseAxios = (baseUrl = BASE_URL) => {
       setLoading(true);
       setError(null);
       try {
+        const headers = endpoint === 'signin' 
+        ? { 'Content-Type': 'application/json', ...addHeaders }
+        : { 
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+            ...addHeaders
+          };
+
         const resp = await axios({
           url: `${baseUrl}${endpoint}`,
           method,
           data: body,
-          headers: {
-            'Content-Type':'application/json',
-            'Authorization':`Bearer ${token}`,
-            ...addHeaders
-          }
+          headers
         });
         setData(resp.data);
         return resp.data; // 이펙트를 쓰지 않아서 리턴 추가
       } catch(err) {
         setError(err);
+        throw err;
       } finally {
         setLoading(false);
       }
