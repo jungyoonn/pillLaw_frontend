06df97541d9b118ad0c3d03ecce9cb2ab6aa6baa
DahnDell <khanman1208@gmail.com>
2025-02-20 18:26:04 +0900

    상품 리스트 컨트롤러와 연결

--- src/component/product/ProductItem.jsx
+++ src/component/product/ProductItem.jsx
@@ -6,18 +6,20 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const ProductItem = ({ product }) => {
   return (
-    <Col xs={6} sm={4} lg={3} xl={2} className="mt-2 mb-4 best-item">
-      <Link to={`/product/detail/${product.id}`} className="text-decoration-none text-black">
-        <img className="img-fluid mx-2" src={product.image} alt={product.name} />
-        <p className="m-0 mt-1 fs-14 fw-bold">{product.name}</p>
+    <Col xs={6} sm={4} lg={3} xl={2} className="mt-2 mb-4">
+      <Link to={`/product/detail/${product.pno}`} className="text-decoration-none text-black">
+        <img className="img-fluid mx-2" src={product.image} alt={product.pname} />
+        <p className="m-0 mt-1 fs-14 fw-bold">{product.pname}</p>
       </Link>
-      <p className="m-0 fs-14 mt-2">
-        <span className="header-font fw-bold">{product.price.toLocaleString()}</span>원
-      </p>
+      {/* <p className="m-0 fs-14 mt-2"> */}
+        {/* <span className="header-font fw-bold">{product.price.toLocaleString()}</span>원 */}
+      {/* </p> */}
       <p className="m-0 fs-12 fw-bold">
-        <FontAwesomeIcon icon={faStar} style={{ color: "#FFD43B" }} /> {product.rating}{" "}
-        <span className="fs-11" style={{ color: "#AAA" }}>
-          리뷰({product.reviews})
+        <FontAwesomeIcon icon={faStar} style={{ color: "#FFD43B" }} /> 
+        {/* {product.rating}{" "} */}
+        <span className="fs-11 text-secondary">
+          리뷰
+          {/* ({product.reviews}) */}
         </span>
       </p>
     </Col>
