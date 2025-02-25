a7fd9129c9832ac9088ce7c8f08b0cabda3deba7
jungyoonn <sophia7625@naver.com>
2025-02-26 00:51:59 +0900

    UseAxios 사용해서 유저 정보 불러오기, 화면에 이메일, 닉네임, 이름 뿌리기

--- src/component/member/myPageChildren/MyInfo.jsx
+++ src/component/member/myPageChildren/MyInfo.jsx
@@ -1,12 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import profile from '../../../resources/image/user-image.png'
-import { Card, Col, Form, Row } from 'react-bootstrap';
+import { Col, Form, Modal, Row } from 'react-bootstrap';
 import Button from '../../common/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoins } from "@fortawesome/free-solid-svg-icons";
+import UseAxios from '../../../hooks/UseAxios';
+import { useNavigate } from 'react-router-dom';
+import logo from '../../../resources/image/pilllaw_favicon.png';
 
 const MyInfo = ({ activeKey }) => {
-  const [email, setEmail] = useState(localStorage.getItem('email'));
+  const [login, setLogin] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [member, setMember] = useState({});
+  const {req} = UseAxios("http://localhost:8080/api");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedEmail = localStorage.getItem("email");
+    const storedMno = localStorage.getItem("mno");
+    setLogin(!!storedMno);
+
+    // 로그인이 안 되어 있으면 모달 표시
+    if (!login) {
+      setShowModal(true);
+    }
+
+    const loadUser = async () => {
+      try {
+        // mno가 있을 때만 API 호출
+        if (storedMno) {
+          const resp = await req('get', `?mno=${storedMno}&email=${storedEmail}`);
+          console.log(resp)
+
+          // 소셜 체크 추가
+          if (resp) {
+            setMember(resp);
+            return;
+          }
+        }
+      } catch (error) {
+        console.error('사용자 정보 로드 실패:', error);
+      }
+    };
+
+    loadUser();
+  }, [login, req])
+
+  const handleCloseAndRedirect = () => {
+    setShowModal(false);
+    navigate('/');
+  };
 
   return (
     <>
@@ -14,7 +57,10 @@ const MyInfo = ({ activeKey }) => {
         <Col xs="2" />
         <Col xs="2" lg="2" className='text-center'>
           <img src={profile} alt='프로필 사진' width={160} />
-          <Button variant='pillllaw' className="btn btn-pilllaw mt-3">PILL LAW 구독</Button>
+          {member ? 
+            <Button variant='pillllaw' className="btn btn-pilllaw mt-3">PILL LAW 구독</Button> : 
+            <p className='fw-bold text-pilllaw fs-12'>소셜 회원은 구독 서비스 이용이 불가능합니다.</p>
+          }
           <p className='fs-14 fw-bold text-pilllaw mt-2'>
             <FontAwesomeIcon icon={faCoins} className='text-pilllaw-secondary mx-1' />
             포인트 : 1500P
@@ -22,31 +68,31 @@ const MyInfo = ({ activeKey }) => {
         </Col>
         <Col xs="1" />
         <Col xs="4">
-          <p className='m-1'>이메일</p>
+          <p className='m-1 fs-14 fw-bold header-font'>이메일</p>
           <Form.Control
             type="text"
-            placeholder={email}
-            className='bg-pilllaw-form'
+            placeholder={member.email || 'USER' + localStorage.getItem("mno")}
+            className='bg-pilllaw-form fs-14 fw-bold'
             disabled
             readOnly
           />
           <Row className='mt-2'>
             <Col>
-              <p className='m-1'>이름</p>
+              <p className='m-1 fs-14 fw-bold header-font'>이름</p>
               <Form.Control
                 type="text"
-                placeholder={email}
-                className='bg-pilllaw-form'
+                placeholder={member.name || 'USER' + localStorage.getItem("mno")}
+                className='bg-pilllaw-form fs-14 fw-bold'
                 disabled
                 readOnly
               />
             </Col>
             <Col>
-              <p className='m-1'>닉네임</p>
+              <p className='m-1 fs-14 fw-bold header-font'>닉네임</p>
               <Form.Control
                 type="text"
-                placeholder={email}
-                className='bg-pilllaw-form'
+                placeholder={member.nickname || member.socialProvider + '_USER'}
+                className='bg-pilllaw-form fs-14 fw-bold'
                 disabled
                 readOnly
               />
@@ -54,6 +100,26 @@ const MyInfo = ({ activeKey }) => {
           </Row>
         </Col>
       </Row>
+      <Modal
+        show={showModal}
+        backdrop="static" // 배경 클릭으로 닫히지 않음
+        keyboard={false} // Esc키로 닫히지 않음
+        centered
+        className='bg-pilllaw-modal'
+      >
+        <Modal.Header className='bg-pilllaw-form'>
+          <Modal.Title className='fw-bold header-font'>
+            <img src={logo} alt='로고' width={30} className='me-3' />
+            PILL LAW
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className='fw-bold text-pilllaw bg-pilllaw-form'>로그인이 필요한 서비스입니다.</Modal.Body>
+        <Modal.Footer className='bg-pilllaw-form'>
+          <Button variant="pilllaw" onClick={handleCloseAndRedirect}>
+            확인
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
