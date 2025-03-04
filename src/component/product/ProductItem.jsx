c2acaac9438be592cce0f2b6bd92eb750fc20d1a
DahnDell <khanman1208@gmail.com>
2025-03-04 17:16:38 +0900

    폼 양식 수정

--- src/component/product/ProductItem.jsx
+++ src/component/product/ProductItem.jsx
@@ -33,13 +33,13 @@ const ProductItem = ({ product, reviews }) => {
     <Col xs={6} sm={4} lg={3} xl={2} className="mt-2 mb-4">
     <Link to={`/product/detail/${product.pno}`} className="text-decoration-none text-black">
       <img className="img-fluid mx-2" style={{height:200}} src={product.imageUrl} alt={product.pname} />
-      <p className="m-0 mt-1 fs-11 fw-bold text-secondary">{product.company}</p>
-      <p className="m-0 mt-1 fs-14 fw-bold">{product.pname}</p>
-      <Row className="m-0 mt-1 mx-2">
+      <p className="m-0 mt-1 fs-11 fw-bold text-secondary text-start mx-2">{product.company}</p>
+      <p className="m-0 mt-1 fs-14 fw-bold text-start mx-2">{product.pname}</p>
+      <Row className="m-0 mt-1">
         {product.priceInfo.rate !== 0 ? (
-          <p className="mb-0 p-0 text-start">
+          <p className="mb-0 p-0 text-start mx-2">
             {product.priceInfo.rate !== 0 && (
-              <span className="text-info fw-bold"> {product.priceInfo.rate} % </span>
+              <span className="text-info fw-bold mx-2"> {product.priceInfo.rate} % </span>
             )}
           </p>
         ) : 
@@ -49,23 +49,21 @@ const ProductItem = ({ product, reviews }) => {
         }
       </Row>
       <Row className="mt-1 mb-0">
-        <Col xs="4" className="text-start p-0">
-          <span className="header-font fw-bold">{product.priceInfo.salePrice.toLocaleString()}</span>원
-        </Col>
-        <Col className="text-start p-0">
-          <span className="header-font text-decoration-line-through text-secondary">
+        <Col>
+          <span className="header-font fw-bold mx-2">{product.priceInfo.salePrice.toLocaleString()}</span>원
+          <span className="mx-2">
             {product.priceInfo.price.toLocaleString()}
           </span>원
         </Col>
-        <Col></Col>
       </Row>
       <Row  className="m-0 p-0 fs-12 fw-bold text-start">
+        <Col>
           <FontAwesomeIcon icon={faStar} style={{ color: "#FFD43B" }} /> 
           {averageRating}
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <span className="fs-12 text-secondary">
+          <span className="fs-12 text-secondary mx-2">
             리뷰 ({reviews.length})
           </span>
+        </Col>
       </Row>
     </Link>
     </Col>
